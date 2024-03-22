66ec9d06febc47e55dd8e4e6e7e414732a54424b
dnuccio80 <nuccio227@gmail.com>
2024-03-22 20:33:24 -0300

    collision detection complete

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,16 +14,60 @@ public class Player : MonoBehaviour
 
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();        
 
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y) * moveSpeed * Time.deltaTime;
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        transform.position += moveDir;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
 
-        float rotationSpeed = 10f;
+        if(!canMove)
+        {
+            // Cannot move towards moveDir
+
+            // Attempt only in X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f);
+
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            } else
+            {
+                // Cannot move only on the X
+
+                // Attempt only Z movement
+                Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z);
+
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                } else
+                {
+                    // Cannot move in any direction
+                }
+
+            }
+
+
+        }
+
+
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+
+
 
         isWalking = moveDir != Vector3.zero;
 
+        float rotationSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
-
         
     }
 
