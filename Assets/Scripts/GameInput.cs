ad69a26bcf741d13124079a57bb9e6a5b2ad4cc5
dnuccio80 <nuccio227@gmail.com>
2024-03-22 19:25:12 -0300

    input system refactor

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+
+    private PlayerInputAction playerInputAction;
+
+    private void Awake()
+    {
+        playerInputAction = new PlayerInputAction();
+        playerInputAction.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized()
+    {
+        Vector2 inputVector = playerInputAction.Player.Move.ReadValue<Vector2>();
+        Debug.Log(inputVector);
+        return inputVector;
+
+    }
+
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,36 +6,13 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] float moveSpeed;
+    [SerializeField] GameInput gameInput;
     private bool isWalking;
 
     void Update()
     {
 
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if(Input.GetKey(KeyCode.W))
-        {
-            inputVector.y += 1f;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y -= 1f;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x -= 1f;
-
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x += 1f;
-
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();        
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y) * moveSpeed * Time.deltaTime;
 
