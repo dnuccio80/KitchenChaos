66e3b60b69c468fe7472c5e150ca0000d1030c82
dnuccio80 <nuccio227@gmail.com>
2024-03-31 21:24:12 -0300

    warningBurning on StoveCounter finished

---
+++ Assets/Scripts/UI/ProgressBarUIAnimation.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressBarUIAnimation : MonoBehaviour
+{
+    private const string IS_FLASHING = "IsFlashing";
+
+    [SerializeField] private StoveCounter stoveCounter;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        stoveCounter.OnProgressChanged += StoveCounter_OnProgressChanged;
+    }
+
+    private void StoveCounter_OnProgressChanged(object sender, IHasProgress.OnProgressChangedEventArgs e)
+    {
+        float burningWarningTime = .5f;
+
+        if (stoveCounter.GetFriedStateIsActive() && e.progressNormalized > burningWarningTime) animator.SetBool(IS_FLASHING, true);
+        else animator.SetBool(IS_FLASHING, false);
+
+    }
+
+}
