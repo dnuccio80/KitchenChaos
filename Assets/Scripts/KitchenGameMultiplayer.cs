df5cfd752131c0fda3ed324a480b2700a31728ab
dnuccio80 <nuccio227@gmail.com>
2024-07-04 14:44:19 -0300

    Sync pickup Object - First Part

---
+++ Assets/Scripts/KitchenGameMultiplayer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class KitchenGameMultiplayer : NetworkBehaviour
+{
+    
+    public static KitchenGameMultiplayer Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent)
+    {
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+        KitchenObject kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+
+        NetworkObject kitchenObjectNetworkObject = kitchenObject.GetComponent<NetworkObject>();
+        kitchenObjectNetworkObject.Spawn(true);
+
+        kitchenObject.SetKitchenObjectParent(kitchenObjectParent);
+    }
+
+}
