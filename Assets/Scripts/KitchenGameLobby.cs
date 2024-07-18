df827e2fce2deb2ecc179b4f01ff757a34965686
dnuccio80 <nuccio227@gmail.com>
2024-07-18 14:33:24 -0300

    Lobby finished

--- Assets/Scripts/KitchenGameLobby.cs
+++ Assets/Scripts/KitchenGameLobby.cs
@@ -16,10 +16,16 @@ public class KitchenGameLobby : MonoBehaviour
     public event EventHandler OnJoinStarted;
     public event EventHandler OnQuickJoinFailed;
     public event EventHandler OnCodeJoinFailed;
+    public event EventHandler<OnLobbyListChangedEventArgs> OnLobbyListChanged;
+    public class OnLobbyListChangedEventArgs : EventArgs
+    {
+        public List<Lobby> lobbyList;
+    }
 
     private Lobby joinedLobby;
 
     private float heartbeatTimer;
+    private float listLobbiesTimer;
 
     private void Awake()
     {
@@ -31,6 +37,22 @@ public class KitchenGameLobby : MonoBehaviour
     private void Update()
     {
         HandleHeartBeat();
+        HandlePeriodicListLobbies();
+    }
+
+    private void HandlePeriodicListLobbies()
+    {
+        if (joinedLobby == null && AuthenticationService.Instance.IsSignedIn)
+        {
+            listLobbiesTimer -= Time.deltaTime;
+
+            if (listLobbiesTimer <= 0)
+            {
+                float listLobbiesTimerMax = 3f;
+                listLobbiesTimer = listLobbiesTimerMax;
+                ListLobbies();
+            }
+        }
     }
 
     private void HandleHeartBeat()
@@ -53,6 +75,29 @@ public class KitchenGameLobby : MonoBehaviour
         
     }
 
+    private async void ListLobbies()
+    {
+        try
+        {
+            QueryLobbiesOptions queryLobbiesOptions = new QueryLobbiesOptions
+            {
+                Filters = new List<QueryFilter> { new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "0", QueryFilter.OpOptions.GT) }
+            };
+
+            QueryResponse queryResponse = await LobbyService.Instance.QueryLobbiesAsync(queryLobbiesOptions);
+
+            OnLobbyListChanged?.Invoke(this, new OnLobbyListChangedEventArgs
+            {
+                lobbyList = queryResponse.Results
+            });
+        } catch (LobbyServiceException e) {
+            Debug.Log(e);
+        }
+        
+
+        
+    }
+    
     private async void InitializeUnityAuthentication()
     {
         // If we back to Main Menu Scene, this GameObject will destroy, and we back to lobby scene, we could have a problem with initialize, so we add a conditional 
@@ -114,6 +159,21 @@ public class KitchenGameLobby : MonoBehaviour
             joinedLobby = await LobbyService.Instance.JoinLobbyByCodeAsync(code);
             KitchenGameMultiplayer.Instance.StartClient();
         } catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+            OnCodeJoinFailed?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public async void JoinWithId(string lobbyId)
+    {
+        OnJoinStarted?.Invoke(this, EventArgs.Empty);
+        try
+        {
+            joinedLobby = await LobbyService.Instance.JoinLobbyByIdAsync(lobbyId);
+            KitchenGameMultiplayer.Instance.StartClient();
+        }
+        catch (LobbyServiceException e)
         {
             Debug.Log(e);
             OnCodeJoinFailed?.Invoke(this, EventArgs.Empty);
--- Assets/Scripts/UI/LobbyUI.cs
+++ Assets/Scripts/UI/LobbyUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Services.Lobbies.Models;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,8 +15,10 @@ public class LobbyUI : MonoBehaviour
     [SerializeField] private TMP_InputField codeInputField;
     [SerializeField] private TMP_InputField playerNameInputField;
     [SerializeField] private LobbyCreateUI lobbyCreateUI;
+    [SerializeField] private Transform lobbyListContainer;
+    [SerializeField] private Transform lobbyTemplate;
 
-    private void Awake()
+    private void Awake() 
     {
         mainMenuButton.onClick.AddListener(() =>
         {
@@ -37,6 +40,8 @@ public class LobbyUI : MonoBehaviour
         {
             KitchenGameLobby.Instance.JoinWithCode(codeInputField.text);
         });
+
+        lobbyTemplate.gameObject.SetActive(false);
     }
 
     private void Start()
@@ -46,7 +51,30 @@ public class LobbyUI : MonoBehaviour
         {
             KitchenGameMultiplayer.Instance.SetPlayerName(newName);
         });
+
+        KitchenGameLobby.Instance.OnLobbyListChanged += KitchenGameLobby_OnLobbyListChanged;
+        UpdateLobbyList(new List<Lobby>());
     }
 
+    private void KitchenGameLobby_OnLobbyListChanged(object sender, KitchenGameLobby.OnLobbyListChangedEventArgs e)
+    {
+        UpdateLobbyList(e.lobbyList);
+    }
 
+    private void UpdateLobbyList(List<Lobby> lobbyList)
+    {
+        foreach( Transform child in lobbyListContainer)
+        {
+            if (child == lobbyTemplate) continue;
+            Destroy(child);
+        }
+
+        foreach(Lobby lobby in lobbyList)
+        {
+            Transform lobbyTransform = Instantiate(lobbyTemplate, lobbyListContainer);
+            lobbyTransform.gameObject.SetActive(true);
+            lobbyTransform.GetComponent<LobbyListSingleUI>().SetLobby(lobby);
+
+        }
+    }
 }
